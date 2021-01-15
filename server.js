b458a6339d7271a7ae871264ceac4e023d9fbc16
Alejandro Radisic <ale.radisic@gmail.com>
2021-01-15 16:13:18 -0300

    processenev

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.prepare().then(() => {
         if (handles)
             handle(req, res, parsedUrl)
 
-    }).listen(3000, (err) => {
+    }).listen(process.env.PORT || 3000, (err) => {
         if (err) throw err
         console.log('> Ready on http://localhost:3000')
     })
